b5f1090df56253f782341026cc58999b2cb09225
Joel Mathew <ruthlessragingraspberry@gmail.com>
2023-02-12 15:52:16 +0530

    added model.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import * as tf from '@tensorflow/tfjs';
+
+async function loadModel() {
+   const model = await tf.loadLayersModel('./model/model.json');
+  console.log('Model loaded.');
+  return model;
+}
+
+
+async function runPrediction(model) {
+  const inputData = tf.tensor2d([[1, 2, 3, 4]], [1, 4]);
+  const output = model.predict(inputData);
+  console.log(output.dataSync());
+}
+
+
+const App = () => {
+  const [model, setModel] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const m = await loadModel();
+      setModel(m);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (model) {
+      runPrediction(model);
+    }
+  }, [model]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TensorFlow.js Example</h1>
     </div>
   );
-}
+};
 
 export default App;
